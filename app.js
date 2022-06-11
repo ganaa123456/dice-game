69bbdc0d20288ba2acbe8d22d23e5320899556c9
ganaa <ganaa.12396@yahoo.com>
2022-06-11 15:04:53 +0800

    app.js d oorchlolt hiiw

---
+++ app.js
@@ -0,0 +1,28 @@
+// toglogchiin eeljig hadgalah huwisagch , 1r toglogchiig 0 , 2r toglogchiig 1 gej temdeglene //
+
+var activePlayer = 1;
+
+// toglogchdiin tsugluulsan onoog hadgalah huwisagch
+
+var scores = [0, 0];
+
+//toglogchiin eeljindee tsugluulj bga onoog hadgalah huwisagch
+
+var roundScore = 0;
+
+//shoonii ali talaaraa buusniig hadgalah huwisagch 1-6
+
+var dice = Math.floor(Math.random() * 6) + 1;
+
+// window.document.querySelector("#score-0").textContent = dice;
+// document.querySelector("#score-1").textContent = dice;
+
+// Program ehlehed beltgeh
+
+document.querySelector("#score-0").textContent = "0";
+document.querySelector("#score-1").textContent = "0";
+
+document.querySelector("#current-0").textContent = "0";
+document.querySelector("#current-1").textContent = "0";
+
+document.querySelector(".dice").style.display = "none";
