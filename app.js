f88e2423caad8888e3725c5ac90f35c5bc83e3c1
ganaa <ganaa.12396@yahoo.com>
2022-06-11 15:43:01 +0800

    roll hiih uyd zurag tuhain toogoor soligddog bolgow

--- app.js
+++ app.js
@@ -12,17 +12,22 @@ var roundScore = 0;
 
 //shoonii ali talaaraa buusniig hadgalah huwisagch 1-6
 
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
 // window.document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").textContent = dice;
 
 // Program ehlehed beltgeh
 
-document.querySelector("#score-0").textContent = "0";
-document.querySelector("#score-1").textContent = "0";
-
-document.querySelector("#current-0").textContent = "0";
-document.querySelector("#current-1").textContent = "0";
-
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 document.querySelector(".dice").style.display = "none";
+
+var diceDom = document.querySelector(".dice");
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
